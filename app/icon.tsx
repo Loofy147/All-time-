b1b4878b2a430edae8f122e8cf2983a5a5b0518c
Loofy <ebh.ceo213@gmail.com>
2026-09-24 20:24:24 +0100

    feat: add app icon

---
+++ app/icon.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 64, height: 64 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#0b1420",
+          color: "#f4f7fb",
+          fontSize: 34,
+          fontWeight: 800,
+          borderRadius: 14,
+        }}
+      >
+        A
+      </div>
+    ),
+    size,
+  );
+}
